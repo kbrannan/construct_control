f5a7751ea94247068460ac90b7bfac79883ab0df
kbrannan <brannan.kevin@deq.state.or.us>
2015-12-08 11:58:00 -0800

    create R script to gather raw data together in one place

---
+++ gather-raw-data-files.R
@@ -0,0 +1,5 @@
+# gather the raw observed data in one place for use in the construction of the
+# PEST control file. Some of the raw data will be processed in other R-projects
+# but I will keep a copy of the raw data used in this repo to increase 
+# transparency
+# created Kevin Brannan 2015-12-08
