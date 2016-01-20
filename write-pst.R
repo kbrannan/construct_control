262e61b94ed39157bc5260a05ddb5177c41668e7
kbrannan <brannan.kevin@deq.state.or.us>
2016-01-20 12:41:20 -0800

    got number of observations using mget and ribind in do.call

--- write-pst.R
+++ write-pst.R
@@ -3,3 +3,13 @@ setwd("m:/models/bacteria/hspf/HydroCal201506/R_Projs/construct_control")
 load(file="obs-blocks.RData")
 
 load("uci-control.RData")
+
+
+head(str.control, 25)
+
+## get number of observation groups
+junk <- rbind(ls(patter = "^df\\..*"))
+
+dfs <- Filter(function(x) is(x, "data.frame"), mget(ls()))
+
+junk <- do.call(rbind,mget(ls(patter = "^df\\..*")))
