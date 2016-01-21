e64b023cd86b3111600059c1c582a310da505f36
kbrannan <brannan.kevin@deq.state.or.us>
2016-01-21 15:37:23 -0800

    explicity set path and added xomments to commands that load output from "gather-raw-data-files.R"

--- write-pst.R
+++ write-pst.R
@@ -1,8 +1,11 @@
-setwd("m:/models/bacteria/hspf/HydroCal201506/R_Projs/construct_control")
+## set path
+chr.dir <- "m:/models/bacteria/hspf/HydroCal201506/R_Projs/construct_control"
 
-load(file="obs-blocks.RData")
+## load the observed data processed by "gather-raw-data-files.R"
+load(file=paste0(chr.dir,"/","obs-blocks.RData"))
 
-load("uci-control.RData")
+## load uci and PEST-control files processed by "gather-raw-data-files.R"
+load(file = paste0(chr.dir, "/", "uci-control.RData"))
 
 
 head(str.control, 25)
@@ -13,3 +16,7 @@ junk <- rbind(ls(patter = "^df\\..*"))
 dfs <- Filter(function(x) is(x, "data.frame"), mget(ls()))
 
 junk <- do.call(rbind,mget(ls(patter = "^df\\..*")))
+
+lil_junk <- junk[sample(nrow(junk),size = 100, replace = TRUE),1]
+
+gsub("^.*")
