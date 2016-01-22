aa5ba130db805234fa8175da17cc3692a2bba81e
kbrannan <brannan.kevin@deq.state.or.us>
2016-01-21 17:09:10 -0800

    have difficulty combining dfs. seems not to be a data.frame???

--- write-pst.R
+++ write-pst.R
@@ -10,8 +10,19 @@ load(file = paste0(chr.dir, "/", "uci-control.RData"))
 ## combine the dfs to form the block of obs 
 lng.og <- grep("\\* observation groups", str.control) + 1
 lng.og.e <- lng.og + grep("\\*", str.control[lng.og:length(str.control)])[1] - 2
+chr.df.names <- paste0("df.",str.control[lng.og:lng.og.e])
 
-paste0("df.",str.control[lng.og:lng.og.e])
+junk <- as.data.frame(do.call(rbind, mget(chr.df.names)))
+
+write(junk[ ,1], file = "junk.txt")
+
+str(junk)
+names(junk)
+
+
+cat(junk, file = "junk.txt", sep="\n")
+
+tail(junk, 100)
 
 ## get number of observation groups
 junk <- rbind(ls(pattern = "^df\\..*"))
