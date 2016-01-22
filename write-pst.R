99fb8c6ecab5dd88e154a5bc05007564d61cd7fd
kbrannan <brannan.kevin@deq.state.or.us>
2016-01-22 12:02:03 -0800

    added new line to control and write to file

--- write-pst.R
+++ write-pst.R
@@ -14,7 +14,6 @@ chr.df.names <- paste0("df.",str.control[lng.og:lng.og.e])
 ## combine blocks
 df.obs.block <- do.call(rbind, mget(chr.df.names))
 
-
 ## get number of obs
 lng.n.obs <- length(df.obs.block$line)
 
@@ -24,14 +23,9 @@ chr.ln <- str.control[4]
 # substitute the new number of observations
 chr.ln.new <- gsub("[0-9]{4,}",as.character(lng.n.obs), chr.ln)
 
+## put new line in control file
+str.control[4] <- chr.ln.new
 
-
-
-
-
-
-system.time(write.table(junk$line, file = "junk.txt", row.names = FALSE, col.names = FALSE,
+## write updated control file
+write.table(str.control, file = "new.pst", row.names = FALSE, col.names = FALSE,
             quote = FALSE)
-)
-
-
